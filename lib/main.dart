50d5232d3647b5e74430bc504abfedfd30ffa168
vitorbrussolozerbato <vitorcsharp@hotmail.com>
2021-05-27 20:15:28 -0300

    Simples navegação entre telas

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:atmconsultoria/HomeNavegacao.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home:ATM(),
+  ));
+}
+
+
+     
